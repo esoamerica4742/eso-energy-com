422fcc6d8130a1e35e0d2663d52f621e29ce18de
esoamerica4742 <esoamerica4742@gmail.com>
2026-05-16 23:22:11 -0400

    Delete register.tsx

--- src/routes/register.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, Navigate } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/register")({
-  component: () => <Navigate to="/signup" replace />,
-});
